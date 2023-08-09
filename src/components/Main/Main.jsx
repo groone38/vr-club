b888da1dc758909f9aafdc39d479180eb3e8a8a0
groone38 <groone38@yandex.ru>
2023-08-09 23:17:27 +0300

    main style done

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <div className="main__info">
+        <div className="main__title">
+          <h1>Клуб виртуальной реальности в Краснодаре</h1>
+        </div>
+        <div className="main__text">
+          <h2>Современная площадка для праздников и развлечений</h2>
+        </div>
+      </div>
+      <div className="main__btn">
+        <button>Забронировать</button>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
