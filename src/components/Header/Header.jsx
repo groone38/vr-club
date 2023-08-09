0943e52f2d669c0635f01d440e1f31cc8d70dedb
groone38 <groone38@yandex.ru>
2023-08-09 22:16:15 +0300

    init commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="!#">Игры</a>
+        </li>
+        <li>
+          <a href="!#"></a>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
